5dfd0b844221af694fc16fa4753ee8f19863fcdd
Kaiqueaq <kaiquesilvaaquino2@gmail.com>
2026-07-02 17:13:50 -0300

    feat: update database configuration and refactor route imports

--- server.js
+++ server.js
@@ -3,10 +3,6 @@
 const app = require('./src/app');
 require('dotenv').config();
 
-
-app.use(cors());
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
